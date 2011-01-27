4dd4c2a111d6f9de3961faa144404a0beca588a7
nan010 <nan010@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-01-27 13:55:28 +0000

    add dependencies

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(name='collective.flexitopic',
       install_requires=[
           'setuptools',
           # -*- Extra requirements: -*-
+          'plone.app.registry',
       ],
       entry_points="""
       # -*- Entry points: -*-
